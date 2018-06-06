c90344c946767413a5469c6e45cd14e618fdd87b
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-06-06 16:09:30 +0200

    Prepare docs for hex.pm

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Membrane Multimedia Framework: MPEG audio format definition
 
 This package provides MPEG audio format definition (so-called caps) for the
-Membrane Multimedia Framework.
+[Membrane Multimedia Framework](https://membraneframework.org).
 
-# Installation
+## Installation
 
 Unless you're developing an Membrane Element it's unlikely that you need to
 use this package directly in your app, as normally it is going to be fetched as
@@ -14,5 +14,5 @@ reason, just add the following line to your `deps` in the `mix.exs` and run
 `mix deps.get`.
 
 ```elixir
-{:membrane_caps_audio_mpeg, git: "git@github.com:membraneframework/membrane-caps-audio-mpeg.git"}
+{:membrane_caps_audio_mpeg, "~> 0.1"}
 ```
--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Membrane.Caps.Audio.MPEG.Mixfile do
       package: package(),
       name: "Membrane Caps: Audio.MPEG",
       source_url: link(),
-      homepage_url: "https://membraneframework.org",
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -19,11 +19,21 @@ defmodule Membrane.Caps.Audio.MPEG.Mixfile do
     "https://github.com/membraneframework/membrane-caps-audio-mpeg"
   end
 
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
   defp package do
     [
       maintainers: ["Membrane Team"],
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => link()}
+      links: %{
+        "GitHub" => link(),
+        "Membrane Framework Homepage" => "https://membraneframework.org"
+      }
     ]
   end
 
